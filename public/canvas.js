46f59587f69fbe45df4f91e78c031a21400c5da1
hursh29 <hurshs0609@gmail.com>
2020-05-30 19:39:09 +0530

    almost there

--- public/canvas.js
+++ public/canvas.js
@@ -1,43 +1,46 @@
-//Variables
-var canvasx = $(canvas).offset().left;
-var canvasy = $(canvas).offset().top;
+var ctx = canvas.getContext('2d'); 
 var last_mousex = last_mousey = 0;
 var mousex = mousey = 0;
 var mousedown = false;
 var tooltype = 'draw'; 
 document.getElementById('erase').addEventListener('click',function(){
+    tooltype = 'erase';
     document.getElementById('mode').innerHTML = 'erase';
 
 }); 
 document.getElementById('draw').addEventListener('click',function(){
     document.getElementById('mode').innerHTML = ' draw ✏';
+    tooltype = 'draw'
 }); 
+
 //Mousedown
-$(canvas).on('mousedown', function(e) {
-    last_mousex = mousex = parseInt(e.clientX-canvasx);
-	last_mousey = mousey = parseInt(e.clientY-canvasy);
+document.querySelector('canvas').addEventListener('mousedown',function(e){
+    last_mousex = mousex = parseInt(e.clientX-this.offsetLeft);
+	last_mousey = mousey = parseInt(e.clientY-this.offsetTop);
     mousedown = true;
 });
 
 //Mouseup
-$(canvas).on('mouseup', function(e) {
+document.querySelector('canvas').addEventListener('mouseup',function(e){
     mousedown = false;
 });
 
 //Mousemove
-$(canvas).on('mousemove', function(e) {
-    mousex = parseInt(e.clientX-canvasx);
-    mousey = parseInt(e.clientY-canvasy);
+document.querySelector('canvas').addEventListener('mousemove',function(e){
+
+    mousex = parseInt(e.pageX-this.offsetLeft);
+    mousey = parseInt(e.pageY-this.offsetTop);
     if(mousedown) {
         ctx.beginPath();
-        if(tooltype=='draw') {
+        if(tooltype == 'draw') {
             ctx.globalCompositeOperation = 'source-over';
             ctx.strokeStyle = 'red';
             ctx.lineWidth = document.getElementById('customRange').value;
 
-        } else {
-            ctx.globalCompositeOperation = 'destination-out';
-            ctx.lineWidth = 15;
+        } 
+        else {
+            ctx.globalCompositeOperation = 'destination-out';            
+            ctx.lineWidth = document.getElementById('customRange').value;
         }
         ctx.moveTo(last_mousex,last_mousey);
         ctx.lineTo(mousex,mousey);
@@ -46,11 +49,5 @@ $(canvas).on('mousemove', function(e) {
     }
     last_mousex = mousex;
     last_mousey = mousey;
-    //Output
-    // $('#output').html('current: '+mousex+', '+mousey+'<br/>last: '+last_mousex+', '+last_mousey+'<br/>mousedown: '+mousedown);
-});
 
-//Use draw|erase
-use_tool = function(tool) {
-    tooltype = tool; //update
-}
+}); 
--- public/fire_base.js
+++ public/fire_base.js
@@ -1,55 +1,47 @@
-var storage = firebase.storage();
-let storageRef = storage.ref();
-let i = 0  ;
-console.log(i,'before')
-// add promises 
-// cors policy error 
-// mouse control
-
-storageRef.child('output/').listAll().then(function( result){
-    // displayImage( i, result.items[i]);
-    i = result.items.length ; 
-}); 
-console.log(i,'after');
-storageRef.child('datasets/').listAll().then(function( result){
-    console.log(i,'displaying');
-    displayImage( i, result.items[i]);
-});
+let i = 0  ; 
+// add promises  
+let output_file_name; 
+let databaseReference =  firebase.database().ref().child('displayImage/currentIndex') ;
+databaseReference.on('value',function (snapshot){
+    i = snapshot.val();
+    firebase.storage().ref().child('input_datasets/').listAll().then(function(result){
+        output_file_name = parseInt(result.items[i].name.split('.')[0]);
+        console.log(i,'displaying',output_file_name );        
+        displayImage( i, result.items[i]);
+    }).catch((err) =>  alert('Error Occured 😑' + `${err}`));
+})
+// database working 
+ 
 var ctx = canvas.getContext('2d');
 let img = new Image();
-var imageWidth, imageHeight;
-img.crossOrigin = 'null';
-img.onload = function(){ 
-    imageWidth = img.naturalWidth ;
-    imageHeight = img.naturalHeight ;
-    // console.log(imageWidth,imageHeight);
-    document.querySelector('canvas').width = imageWidth;
-    document.querySelector('canvas').height = imageHeight;
-    ctx.drawImage(img,0,0,imageWidth,imageHeight);
-};  
-
+var imageWidth = 600, imageHeight = 400 ;
 document.querySelector('#submit').addEventListener('click',function(){
-    // i++ ;
-    const imageDataURL = canvas.toDataURL('image/jpeg') ;
-    console.log(imageDataURL);
-    const ref = firebase.storage().ref();
-    ref.child( 'output/'+  `${new Date()}-base64`).putString(imageDataURL,'data_url').then(function(snapshot){
-        alert('Image submitted! Thanks for your response 😊')
-    });
-    setTimeout(function(){
-        location.reload();
-    },2000);
-    // document.querySelector('#canvasSaved').src = imageDataURL;
-    // location.reload();
-});
 
+        const imageDataURL = canvas.toDataURL('image/jpeg') ;
+        // console.log(imageDataURL);
+        let path = 'output/' + `${output_file_name}/` + `output_${output_file_name}_` + `${new Date()}` ;
+        firebase.storage().ref().child(path).putString(imageDataURL,'data_url').then(function(snapshot){
+            firebase.database().ref().child('displayImage').update({'currentIndex': i+1})
+            alert('Image submitted!! Thanks for your response 😊')
+            
+            location.reload();
+        }).catch(() => alert('Oops !!! Data not submitted 😣'));
+})
+
+img.onload = function(){
+    img.crossOrigin = 'anonymous'
+    ctx.drawImage( img, 0, 0, 600, 400);
+}
 function displayImage( row, images){
     images.getDownloadURL().then(function(url){
-        var str = '<img src="' + url + '">';
+        const URL = url ;
+        var str = '<img src="' + URL + '">';
+        img.src = URL ;
+        
+        document.querySelector("canvas").style.backgroundImage = 'URL("' + img.src + '")';
+        document.querySelector('canvas').style.backgroundSize = "600px 400px"
 
-        img.src = url ;
-        document.querySelector("canvas").style.backgroundImage = 'url("' + img.src + '")';
-      
         console.log(url);
     });
-}
+}    
+   
