55f17eb29ff998daab0da12e69b68ee3b1d7785f
hursh29 <hurshs0609@gmail.com>
2020-06-03 01:48:53 +0530

    screen touch not working

--- public/canvas.js
+++ public/canvas.js
@@ -4,9 +4,8 @@ var mousex = mousey = 0;
 var mousedown = false;
 var tooltype = 'draw'; 
 document.getElementById('erase').addEventListener('click',function(){
-    tooltype = 'erase';
     document.getElementById('mode').innerHTML = 'erase';
-
+    tooltype = 'erase';
 }); 
 document.getElementById('draw').addEventListener('click',function(){
     document.getElementById('mode').innerHTML = ' draw ✏';
@@ -14,40 +13,41 @@ document.getElementById('draw').addEventListener('click',function(){
 }); 
 
 //Mousedown
-document.querySelector('canvas').addEventListener('mousedown',function(e){
-    last_mousex = mousex = parseInt(e.clientX-this.offsetLeft);
-	last_mousey = mousey = parseInt(e.clientY-this.offsetTop);
-    mousedown = true;
-});
+$('canvas').on('mousedown touchdown',function(e){
+        last_mousex = mousex = parseInt(e.clientX-this.offsetLeft);
+        last_mousey = mousey = parseInt(e.clientY-this.offsetTop);
+        // e.preventDefault();
+        mousedown = true;
+    })  
 
 //Mouseup
-document.querySelector('canvas').addEventListener('mouseup',function(e){
-    mousedown = false;
-});
-
+$('canvas').on('mouseup touchup',function(e){
+        // e.preventDefault();
+        mousedown = false;
+    })
 //Mousemove
-document.querySelector('canvas').addEventListener('mousemove',function(e){
-
-    mousex = parseInt(e.pageX-this.offsetLeft);
-    mousey = parseInt(e.pageY-this.offsetTop);
-    if(mousedown) {
-        ctx.beginPath();
-        if(tooltype == 'draw') {
-            ctx.globalCompositeOperation = 'source-over';
-            ctx.strokeStyle = 'red';
-            ctx.lineWidth = document.getElementById('customRange').value;
-
-        } 
-        else {
-            ctx.globalCompositeOperation = 'destination-out';            
-            ctx.lineWidth = document.getElementById('customRange').value;
+$('canvas').on('mousemove touchmove',function(e){
+        // e.preventDefault();
+        mousex = parseInt(e.pageX-this.offsetLeft);
+        mousey = parseInt(e.pageY-this.offsetTop);
+        if(mousedown) {
+            ctx.beginPath();
+            if(tooltype == 'draw') {
+                ctx.globalCompositeOperation = 'source-over';
+                ctx.strokeStyle = 'red';
+                ctx.lineWidth = document.getElementById('customRange').value;
+    
+            } 
+            else {
+                ctx.globalCompositeOperation = 'destination-out';            
+                ctx.lineWidth = document.getElementById('customRange').value;
+            }
+            ctx.moveTo(last_mousex,last_mousey);
+            ctx.lineTo(mousex,mousey);
+            ctx.lineJoin = ctx.lineCap = 'round';
+            ctx.stroke();
         }
-        ctx.moveTo(last_mousex,last_mousey);
-        ctx.lineTo(mousex,mousey);
-        ctx.lineJoin = ctx.lineCap = 'round';
-        ctx.stroke();
-    }
-    last_mousex = mousex;
-    last_mousey = mousey;
-
-}); 
+        last_mousex = mousex;
+        last_mousey = mousey;
+    
+    })
--- public/fire_base.js
+++ public/fire_base.js
@@ -1,12 +1,23 @@
 let i = 0;
 let output_file_name;
 let maxSubmit = 10;
+console.log( 'js', window.innerWidth, window.innerHeight) ;
 var ctx = canvas.getContext("2d");
 let loading = new Image();
 loading.src = 'loading.png';
-ctx.drawImage(loading, 0, 0, 600, 400)
-
-let img = new Image(); 
+let canvasHeight,canvasWidth;
+// canvasWidth = 600;
+// canvasHeight = 400; 
+canvasWidth = 0.44*window.innerWidth;
+canvasHeight = 0.6667*canvasWidth;
+// canvasWidth = Math.min(600.0,canvasWidth);
+// canvasHeight = Math.min(400.0,canvasHeight);
+ctx.canvas.width = canvasWidth ;
+ctx.canvas.height = canvasHeight; 
+console.log(canvasWidth ,canvasHeight,'#canvas'); 
+document.querySelector('canvas').style.backgroundImage = 'URL(loading.png)' 
+document.querySelector("canvas").style.backgroundSize =`${canvasWidth}px ${canvasHeight}px`
+        
 let databaseReference = firebase.database().ref().child("displayImage/currentIndex");
 databaseReference.on("value", function (snapshot) {
     i = snapshot.val();
@@ -23,27 +34,19 @@ databaseReference.on("value", function (snapshot) {
 });
 // database working
 let userSession = 0;
-
-var imageWidth = 600,
-    imageHeight = 400;
-
 document.querySelector("#submit").addEventListener("click", function () {
     userSession++;
     const imageDataURL = canvas.toDataURL("image/jpeg");
     // console.log(imageDataURL);
-    let path =
-        "output/" +
-        `${output_file_name}/` +
-        `output_${output_file_name}_` +
-        `${new Date()}`;
+    let path ="output/" +`${output_file_name}/`+`output_${output_file_name}_` +`${new Date()}`;
     firebase.storage().ref()
         .child(path)
         .putString(imageDataURL, "data_url")
         .then(function (snapshot) {
             firebase.database().ref()
-                .child("displayImage")
-                .update({ currentIndex: i + 1 });
+                .child("displayImage").update({ currentIndex: i + 1 });
             console.log("image session", userSession);
+ 
             document.getElementById("moreToGo").innerHTML = maxSubmit - userSession;
             if (userSession == maxSubmit - 1) {
                 document.querySelector("#submit").value = "submit";
@@ -52,29 +55,34 @@ document.querySelector("#submit").addEventListener("click", function () {
                 alert("Images submitted!! Thanks for your response 😊");
                 window.location = window.location.href
             }
-            ctx.clearRect(0, 0, 600, 400);
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            document.querySelector("canvas").style.backgroundImage = 'URL("' + 'loading.png' + '")' 
+            document.querySelector("canvas").style.backgroundSize =`${canvasWidth}px ${canvasHeight}px`
+        
             // img.src = 'loading.png';
         })
         .catch(() => alert("Oops !!! Data not submitted 😣"));
 });
-img.onload = function () {
+let img = new Image();
+img.onload = function() { 
+    console.log('0image',this.width,this.height);
     img.crossOrigin = "anonymous";
-    ctx.drawImage(img, 0, 0, 600, 400);
-    console.log("session", userSession);
-}; 
+    ctx.drawImage(img, 0, 0, 316, 211);  
+};   
+    
 function displayImage(row, images) {
     images.getDownloadURL().then(function (url) {
         const URL = url;
         var str = '<img src="' + URL + '">';
-        ctx.clearRect(0, 0, 600, 400);
-        img.src = URL;
-
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        img.src = URL; 
+    
         document.querySelector("canvas").style.backgroundImage =
             'URL("' + img.src + '")';
-        document.querySelector("canvas").style.backgroundSize = "600px 400px";
+        document.querySelector("canvas").style.backgroundSize =`${canvasWidth}px ${canvasHeight}px`
         // console.log(url);
     });
 }
 function eraseAll() {
-    ctx.clearRect(0, 0, 600, 400);
-}
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+} 
